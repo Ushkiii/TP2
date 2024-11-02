f68394cc8c1a9a2753b30a082a672b311ca66282
Ushkiii <egorboroda20@gmail.com>
2024-11-02 14:40:53 +0300

    second commit

---
+++ Worker.cpp
@@ -0,0 +1,4 @@
+#include "Worker.h"
+#include <iostream>
+#include <string>
+using namespace std;
---
+++ Worker.h
@@ -0,0 +1,39 @@
+#ifndef _WORKER_H_
+#define _WORKER_H_
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Worker
+{
+    private:
+        string NameWorker;
+        string JobTitle;
+        int JobYear;
+
+    public:
+        Worker();
+        Worker(const Worker&);
+        Worker(string& inNameWorker, string& inJobTitle, int& inJobYear);
+        ~Worker();
+
+        void Print() const;
+        void Save(ostream&) const;
+        void Load(istream&);
+        bool Menu();
+
+        string GetNameWorker();
+        string GetJobTitle();
+        int GetJobYear();
+
+        void SetNameWorker(string InNameWorker );
+        void SetJobTitle(string inJobTitile);
+        void SetJobYear(int inJobTitle);
+
+
+
+};
+
+
+#endif
