664c8120c7144fcd9f9c7fec33dabd47df467d99
Ushkiii <egorboroda20@gmail.com>
2024-11-06 01:48:44 +0300

    third commit

--- Worker.cpp
+++ Worker.cpp
@@ -2,3 +2,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+Worker::Worker(){cout<<"The constructor"<<endl;}
+
+Worker::Worker(const Worker& worker)
+{
+    NameWorker=worker.NameWorker;
+    JobTitle=worker.JobTitle;
+    JobYear=worker.JobYear;
+    cout<<"The copy construct"<<endl;
+}
+
+Worker::Worker(string&inNameWorker, string& inJobTitle, int& inJobYear)
+{
+    NameWorker=inNameWorker;
+    JobTitle=inJobTitle;
+    JobYear=inJobYear;
+    cout<<"The constructor with parametrs"<<endl;
+}
+
+Worker::~Worker(){cout<<"The destructor"<<endl;}
